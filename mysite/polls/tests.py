07938e325ac1cfb36afc562d6a7969f9b6927d8a
MasaGt <MasaGt@users.noreply.github.com>
2022-02-09 11:27:24 +1300

    Added test cases on models

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
+from polls.models import Question
 
 # Create your tests here.
+
+
+class QuestionModelTests(TestCase):
+    def test_was_published_with_future_date(self):
+        """
+        question with future date should be returned False
+        when was_published is call
+        """
+        future_date = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=future_date)
+        self.assertIs(future_question.was_published(), False)
+
+    def test_was_published_with_old_question(self):
+        """
+        quetion with past date should be returned True
+        when was_published is call
+        """
+        time = timezone.now() - datetime.timedelta(days=1)
+        old_quetion = Question(pub_date=time)
+        self.assertIs(old_quetion.was_published(), True)
